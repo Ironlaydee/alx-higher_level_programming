6e109b5ad05732055c0c1835174b19dd25b77e67
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-14 07:08:33 -0700

    we arguing

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const count = process.argv.length 
+if argv === 2 { 
+	console.log('No argument');} 
+else if (process.argv.length === 5) { 
+	console.log('Argument found');} 
+else { 
+	console.log('Arguments found');}
