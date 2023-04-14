d2ee7be3b79ae93bc952d888d316f358cb0b667f
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-14 06:35:21 -0700

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-
 // A script that prints 3 lines
-first line = 'C is fun'
-sexond line = 'Python is cool'
-third line = 'JavaScript is amazing'
 
 console.log('C is fun)
 console.log('Python is cool')
