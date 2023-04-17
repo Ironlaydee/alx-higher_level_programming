37353d46289a5c71ba311b9eaeaa699f1d6cf73e
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-17 08:29:25 +0100

    all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python
 
 // A script that lists all states from the database hbtn_0e_0_usa
+//
 
 import sys
 import MySQLdb
-
 
 if __name__ == "__main__";
   db = MySQLdb.connect(user=sys.argv[1],
@@ -19,5 +18,7 @@ if __name__ == "__main__";
 
   for state in allStates:
       print(state)
-
+      
+      c.close()
+      db.close()
 
