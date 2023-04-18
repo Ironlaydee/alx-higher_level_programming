f44daa0ba30160e11ad1811c2dfb13f59c1c1924
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-18 03:10:25 -0700

    double argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
-// If no arguments, print "No argument", if argument found print "Argument found"
- Otherwise print "Arguments Found"
+// If there is no arguments, print "No argument", if argument found print "Argument found"
+// Otherwise print "Arguments Found"
  if (process.argv.length === 2) {
-
    console.log('No argument');
 } else if (process.argv.length === 3) {
    console.log('Argument found');
