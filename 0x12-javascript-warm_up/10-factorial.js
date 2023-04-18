ffa2973a4acc28b88fc991f5873b24af6e87bb09
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-18 16:25:29 +0100

    factorial of  NaN

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,10 @@
 const myArgs = process.argv.slice(2);
 function factorial (num) {
   if (isNaN(myArgs[0])) {
-   return 1;
+    return 1;
   }
   if (num <= 1) {
-   return 1;
+    return 1;
   }
   return num * factorial(num - 1);
 }
