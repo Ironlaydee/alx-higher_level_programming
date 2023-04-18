95fc0cb4cb3d7222b94a56b9d11bdd4ed9035794
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-18 13:20:54 +0100

    the value arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@
 // If no arguments are passed to the script, print "No argument"
 const myArgs = process.argv.slice(2);
 if (!myArgs[0]) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-       console.log(myArgs[0]);
+  console.log(myArgs[0]);
 }
