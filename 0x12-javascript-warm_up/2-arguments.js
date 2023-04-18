10256ee8f04acccff6cdd3f5aa9f5d3936ea0412
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-18 13:22:26 +0100

    double arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // If there is no arguments, print "No argument", if argument found print "Argument found"
 // Otherwise print "Arguments Found"
  if (process.argv.length === 2) {
-   console.log('No argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-   console.log('Argument found');
+  console.log('Argument found');
 } else {
-   console.log('Arguments found');
+  console.log('Arguments found');
 }
