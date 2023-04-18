5ff618b033b733f0a9e06c7ec606a6e188bbd16a
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-18 13:18:58 +0100

    multi langs

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // prints 3 lines, but by using an array of string and a loop
 for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	console.log(line);
+  console.log(line);
 }
