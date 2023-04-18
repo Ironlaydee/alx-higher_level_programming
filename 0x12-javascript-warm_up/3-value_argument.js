6f79db2b09f0686a1c177601ffb73a68516bb0b8
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-18 03:47:35 -0700

    its value argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// A script that prints the first argument passed to it:
+// If no arguments are passed to the script, print "No argument"
+
+const myArgs = process.argv.slice(2);
+if (!myArgs[0]) {
+	console.log('No argument');
+} else {
+	console.log(myArgs[0]);
+}
