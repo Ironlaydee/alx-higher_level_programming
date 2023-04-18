75732b3f2b6b8bf356a8b7aafce3a2cbb9bf063b
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-17 17:09:55 -0700

    cities by st

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A script that list all cities from a database
+A script that list all cities from a database 'hbtn_0e_4_usa'
 """
 
 import sys
@@ -11,8 +11,8 @@ if __name__ == '__main__':
                          db=sys.argv[3], port=3306, host="localhost")
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id;")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \ 
+            JOIN states ON cities.state_id = states.id;")
     states = cur.fetchall()
 
 for state in states:
