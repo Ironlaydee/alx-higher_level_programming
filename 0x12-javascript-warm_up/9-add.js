614b8907eb936b91a77c71eb72e828cc2aff2cbe
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-18 06:37:02 -0700

    javascript addup

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/nodes
+// a script that prints addition of 2 integers, 1 argument, 1 integer: 2 argument, 2 integer
+// function with this prototype: function add(a, b)
+const myArgs = process.argv.slice(2);
+let result = 0;
+function add (a, b) {
+  result = a + b;
+  return result;
+}
+add(parseInt(myArgs[0]), parseInt(myArgs[1]));
+console.log(result);
