93ed281112467858bc01d913e9cf2577762de951
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-18 04:44:20 -0700

    interger5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// a script that prints My number: if can’t be converted to an integer, print "Not a number"
+const num = Math.floor(Number(process.argv[2]))
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
