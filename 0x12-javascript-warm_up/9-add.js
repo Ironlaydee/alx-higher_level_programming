a077a6c8bd1945fa34606a676427687ff5e28ec5
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-18 16:44:38 +0100

    addition again

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,4 @@
-#!/usr/bin/nodes
+#!/usr/bin/node
 // a script that prints addition of 2 integers, 1 argument, 1 integer: 2 argument, 2 integer
 // function with this prototype: function add(a, b)
 const myArgs = process.argv.slice(2);
@@ -7,5 +7,5 @@ function add (a, b) {
   result = a + b;
   return result;
 }
-add(parseInt(myArgs[4]), parseInt(myArgs[5]));
+add(parseInt(myArgs[0]), parseInt(myArgs[1]));
 console.log(result);
