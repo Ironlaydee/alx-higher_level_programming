0f6e73082980c2349aef78efdd4155d2878c031a
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-19 13:39:09 +0100

    filter the city

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,8 +2,8 @@
 """
 A script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
 """
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
