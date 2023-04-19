c67fddadab02871c8c30c634dbd3c989d77c2434
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-19 05:04:00 -0700

    python ORM task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
+""" A script that list all 'states' frkm the database 'hbtn_0_0e_usa:
 """
-A script lists all states from the database hbtn_0e_0_usa
-"""
-
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    list_all()
+if __name__ == '__main__': 
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[                         2], db=sys.argv[3], port=3306, host='localhost')
 
-def list_all():
-        '''list all states in db'''
-        username = sys.argv[1], password = sys.argv[2], db_name = sys.argv[3], host = 'localhost', port = 3306
-            db = MySQLdb.connect(user=username, host=localhost, passwd=password, db=db_name, port=port)
-        cur = db.cursor()
-        cur.execute('SELECT * FROM states ORDER BY id ASC;')
-        result = cur.fetchall()
-        cur.close()
-        db.close()
- if result: for row in result:
-        print(row)
+    cur = db.cursor() 
+    cur.execute("SELECT * FROM states;") 
+    states = cur.fetchall() 
+
+    for state in states:
+        print(state)
