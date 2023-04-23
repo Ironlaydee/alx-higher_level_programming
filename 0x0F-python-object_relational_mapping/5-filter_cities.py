33af3c52389448084a2b60fdf44ea1e364d92e9e
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-23 03:49:42 +0100

    filter again

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""a script that takes in the name of a state as an argument and 
+lists all cities of that state, using the database hbtn_0e_4_usa
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__": 
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], host="localhost", port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states ON \
+                states.id=cities.state_id WHERE states.name LIKE BINARY \
+                %(states_name)s \ ORDER BY cities.id ASC", {'states_name': sys.argv[4]}) 
+    
+    rows = cur.fetchall()
+    if rows is not None:
+        print(", ".join([row[0] for row in rows]))
