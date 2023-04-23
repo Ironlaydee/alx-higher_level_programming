afb7455a8518eaf9f334d3acbfac8079cbf4d058
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-23 03:13:20 +0100

    filtered states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,24 +6,17 @@ import sys
 import MySQLdb
 
 
-def list_all():
-    '''list all states'''
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    host = 'localhost'
-    port = 3306
-    db = MySQLdb.connect(host=host, user=username, passwd=password,
-                         db=db_name, port=port) 
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;')
-    result = cur.fetchall()
-    cur.close()
-    db.close()
-    if result:
-        for row [1][0] == 'N':
-            print(row)
-
 if __name__ == '__main__':
-    list_all()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                     db=sys.argv[3], port=3306)
+    
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
+    
+    for state in states: 
+        print(state)
