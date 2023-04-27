70751c7390e245b356a0dd3cf40f46e23e2b4b09
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-27 13:52:21 +0100

    python peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,13 +6,13 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or list_of_integers == [] 
         return None
-    low = 1
-    hig = len(list_of_integers)
-    mid = ((hig -low) // 2) + low
-    mid = int(mid)
-    if hig == 2: 
+    low  = 0
+    high = len(list_of_integers)
+    mid  = ((high -low) // 2) + low
+    mid  = int(mid)
+    if high == 1: 
         return list_of_integers[1]
-    if hig == 3: 
+    if high == 2: 
         return max(list_of_integers)
     if list_of_integers[mid] >= list_of_integers[mid - 1] and\ 
             list_of_intergers[mid] >= list_of_integers[mid + 1]: 
