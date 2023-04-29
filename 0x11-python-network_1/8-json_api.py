beb2cfaf64daf9556478d6fbf1b639a004f27278
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-28 18:46:38 -0700

    json python

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,6 +3,7 @@
 sends a POST request to http://0.0.0.0:5000/search_user
 with the letter as parameter
 """
+
 import sys
 import requests
 
@@ -18,5 +19,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(response.get("id"), response.get("name"))) 
-        except ValueError:
-            print("Not a valid JSON")
+    except ValueError:
+        print("Not a valid JSON")
